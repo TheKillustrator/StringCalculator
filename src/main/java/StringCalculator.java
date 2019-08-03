cce42eabf491c3182885d98a573656b8a215d7da
Lindsay Parker <thekillustrator@gmail.com>
2019-08-02 22:32:44 -0400

    green: test case for 1

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,6 +1,12 @@
+import org.apache.commons.lang3.StringUtils;
+
 public class StringCalculator {
 
-    public int add(String input){
-        return 0;
+    public int add(String input) {
+        if (StringUtils.isNotEmpty(input)) {
+            return Integer.parseInt(input);
+        } else {
+            return 0;
+        }
     }
 }
