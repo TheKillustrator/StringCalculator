4e59ab4a71614cd7f78673108e596eda89673b40
Lindsay Parker <thekillustrator@gmail.com>
2019-08-03 12:03:52 -0400

    green: allow specification of delimiter

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -9,7 +9,16 @@ public class StringCalculator {
         int sum = 0;
 
         if (StringUtils.isNotEmpty(input)) {
-            List<String> addends = Arrays.asList(input.split(",|\n"));
+
+            String delimiterRegex = ",|\n";
+            String addendString = input;
+
+            if (input.startsWith("//")) {
+                delimiterRegex = input.substring(input.indexOf("//")+2, input.indexOf("\n"));
+                addendString = input.substring(input.indexOf("\n")+1);
+            }
+
+            List<String> addends = Arrays.asList(addendString.split(delimiterRegex));
             for (String addend : addends) {
                 sum += Integer.parseInt(addend);
             }
