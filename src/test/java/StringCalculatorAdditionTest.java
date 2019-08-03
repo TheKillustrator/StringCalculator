6f8a9116aed0e8b3e06a40360dbe334bdc1595d9
Lindsay Parker <thekillustrator@gmail.com>
2019-08-02 22:19:05 -0400

    refactor for parameterized tests

---
+++ src/test/java/StringCalculatorAdditionTest.java
@@ -0,0 +1,38 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class StringCalculatorAdditionTest {
+
+    private StringCalculator calculator;
+    private String input;
+    private int expectedSum;
+
+    public StringCalculatorAdditionTest(String input, int expectedSum) {
+        this.input = input;
+        this.expectedSum = expectedSum;
+    }
+
+    @Parameterized.Parameters(name = "Test {index}: input={0}, expected sum={1}")
+    public static Iterable<Object[]> data() throws Throwable {
+        return Arrays.asList(new Object[][]{
+                {"", 0}
+        });
+    }
+
+    @Before
+    public void setUp() {
+        calculator = new StringCalculator();
+    }
+
+    @Test
+    public void addReturnsTheCorrectSum() {
+        assertEquals("Incorrect sum returned", expectedSum, calculator.add(input));
+    }
+}
--- src/test/java/StringCalculatorTests.java
+++ src/test/java/StringCalculatorTests.java
@@ -1,7 +1,6 @@
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class StringCalculatorTests {
@@ -17,10 +16,5 @@ public class StringCalculatorTests {
     public void addTakesStringAndReturnsAnInt(){
         int sum = Integer.valueOf(calculator.add(""));
         assertNotNull(sum);
-    }
-
-    @Test
-    public void addReturnsTheCorrectSum(){
-        assertEquals("Incorrect sum returned", 0, calculator.add(""));
     }
 }
