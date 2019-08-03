132d0f342e837163cc5ac6deb82a72b21fbee361
Lindsay Parker <thekillustrator@gmail.com>
2019-08-03 14:02:25 -0400

    note for another edge case

--- src/test/java/StringCalculatorAdditionTest.java
+++ src/test/java/StringCalculatorAdditionTest.java
@@ -36,6 +36,7 @@ public class StringCalculatorAdditionTest {
                 {"//[***]\n1***2***3", 6}
 
                 //TODO edge case "//\n\n1\n2" = 3, is valid? exception?
+                //TODO are multi-character delimiters not enclosed in [] invalid?
         });
     }
 
