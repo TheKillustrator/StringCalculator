eeb3df30b319573e085f2105b47f541302fe69b2
Lindsay Parker <thekillustrator@gmail.com>
2019-08-02 22:47:24 -0400

    green: test case for 1,2

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,10 +1,18 @@
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StringCalculator {
 
     public int add(String input) {
         if (StringUtils.isNotEmpty(input)) {
-            return Integer.parseInt(input);
+            List<String> addends = Arrays.asList(input.split(","));
+            int sum = 0;
+            for (String addend : addends){
+                sum += Integer.parseInt(addend);
+            }
+            return sum;
         } else {
             return 0;
         }
