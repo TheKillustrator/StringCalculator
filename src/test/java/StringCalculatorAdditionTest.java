ff07d4dc35a3380f2ee8d528f599c942f23fdf3a
Lindsay Parker <thekillustrator@gmail.com>
2019-08-03 13:24:43 -0400

    red: test to ignore numbers >1000

--- src/test/java/StringCalculatorAdditionTest.java
+++ src/test/java/StringCalculatorAdditionTest.java
@@ -29,7 +29,8 @@ public class StringCalculatorAdditionTest {
                 {"1,2,3,4", 10},
                 {"1\n2,3", 6},
                 {"//;\n1;2", 3},
-                {"//and\n1and2", 3}
+                {"//and\n1and2", 3},
+                {"1001,2", 2}
 
                 //TODO edge case "//\n\n1\n2" = 3, is valid? exception?
         });
